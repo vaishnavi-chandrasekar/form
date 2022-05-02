2959b7476dafdf337daad82c234c22fec0d5b0b4
vaishnavi <vaishnavichandrasekar123@gmail.com>
2022-05-02 19:23:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import pic from "./images/nature.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <div className="App">  
+      <div className='form'>
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="Username" required></input><br />
+          <input type="password" placeholder='password'  required></input><br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+       
+      </div>
+   </>
   );
 }
 
 export default App;
+
+
+//what is react ?
+//elements and components
+//  <h1>,<div>,<img> = elements
+//  position properties:
+//    relative
+//absolute
+//sticky
+//fixed
+//static  = default
